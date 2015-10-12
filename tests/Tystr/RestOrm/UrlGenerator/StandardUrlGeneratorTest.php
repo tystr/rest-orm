79fc92dedca87a28f10083dedf22278313f738da
Tyler Stroud <tyler@tylerstroud.com>
2015-10-12 15:46:51 -0700

    Handle required url variables like /categories/{{categoryId}}/posts

--- tests/Tystr/RestOrm/UrlGenerator/StandardUrlGeneratorTest.php
+++ tests/Tystr/RestOrm/UrlGenerator/StandardUrlGeneratorTest.php
@@ -15,46 +15,107 @@ class StandardUrlGeneratorTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider getParameters
+     * @dataProvider getData
      */
-    public function testGetCreateUrl($parameters, $expectedQueryString)
+    public function testGetCreateUrl($resource, $parameters, $expectedQueryString)
     {
-        $url = $this->generator->getCreateUrl('cars', $parameters);
+        $url = $this->generator->getCreateUrl($resource, $parameters);
         $this->assertEquals('/cars'.$expectedQueryString, $url);
     }
 
     /**
-     * @dataProvider getParameters
+     * @dataProvider getData
      */
-    public function testGetModifyUrl($parameters, $expectedQueryString)
+    public function testGetModifyUrl($resource, $parameters, $expectedQueryString)
     {
-        $url = $this->generator->getModifyUrl('cars', 42, $parameters);
+        $url = $this->generator->getModifyUrl($resource, 42, $parameters);
         $this->assertEquals('/cars/42'.$expectedQueryString, $url);
     }
 
     /**
-     * @dataProvider getParameters
+     * @dataProvider getData
      */
-    public function testGetFindOneUrl($parameters, $expectedQueryString)
+    public function testGetFindOneUrl($resource, $parameters, $expectedQueryString)
     {
-        $url = $this->generator->getFindOneUrl('cars', 12, $parameters);
+        $url = $this->generator->getFindOneUrl($resource, 12, $parameters);
         $this->assertEquals('/cars/12'.$expectedQueryString, $url);
     }
 
     /**
-     * @dataProvider getParameters
+     * @dataProvider getData
      */
-    public function testGetFindAllUrl($parameters, $expectedQueryString)
+    public function testGetFindAllUrl($resource, $parameters, $expectedQueryString)
     {
-        $url = $this->generator->getFindAllUrl('cars', $parameters);
+        $url = $this->generator->getFindAllUrl($resource, $parameters);
         $this->assertEquals('/cars'.$expectedQueryString, $url);
     }
 
-    public function getParameters()
+    /**
+     * @dataProvider getDataWithUrlRequirements
+     */
+    public function testGetCreateUrlWithUrlRequirements($resource, $parameters, $requirements,  $expectedUrl, $expectedQueryString)
+    {
+        $url = $this->generator->getCreateUrl($resource, $parameters, $requirements);
+        $this->assertEquals($expectedUrl.$expectedQueryString, $url);
+    }
+
+    /**
+     * @dataProvider getDataWithUrlRequirements
+     */
+    public function testGetModifyUrlWithUrlRequirements($resource, $parameters, $requirements,  $expectedUrl, $expectedQueryString)
+    {
+        $url = $this->generator->getModifyUrl($resource, 42, $parameters, $requirements);
+        $this->assertEquals($expectedUrl.'/42'.$expectedQueryString, $url);
+    }
+
+    /**
+     * @dataProvider getDataWithUrlRequirements
+     */
+    public function testGetFindOneWithUrlRequirements($resource, $parameters, $requirements,  $expectedUrl, $expectedQueryString)
+    {
+        $url = $this->generator->getFindOneUrl($resource, 42, $parameters, $requirements);
+        $this->assertEquals($expectedUrl.'/42'.$expectedQueryString, $url);
+    }
+
+    /**
+     * @dataProvider getDataWithUrlRequirements
+     */
+    public function testGetFindAllWithUrlRequirements($resource, $parameters, $requirements,  $expectedUrl, $expectedQueryString)
+    {
+        $url = $this->generator->getFindAllUrl($resource, $parameters, $requirements);
+        $this->assertEquals($expectedUrl.$expectedQueryString, $url);
+    }
+
+    public function getData()
     {
         return [
-            [[], ''],
-            [['limit' => 10, 'page' => 5], '?limit=10&page=5'],
+            ['cars', [], ''],
+            ['cars', ['limit' => 10, 'page' => 5], '?limit=10&page=5'],
+        ];
+    }
+
+    public function getDataWithUrlRequirements()
+    {
+        $requirements = [
+            'categoryId' => 'hello',
+            'postId' => 'world',
+        ];
+
+        return [
+            [
+                'blogs/{{categoryId}}/posts/{{postId}}',
+                [],
+                $requirements,
+                '/blogs/hello/posts/world',
+                '',
+            ],
+            [
+                'blogs/{{categoryId}}/posts/{{postId}}',
+                ['limit' => 10, 'page' => 5],
+                $requirements,
+                '/blogs/hello/posts/world',
+                '?limit=10&page=5',
+            ],
         ];
     }
 }
