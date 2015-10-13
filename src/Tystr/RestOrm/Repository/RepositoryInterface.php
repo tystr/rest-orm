db479a6e5f3a261a082b9440020331eef34126c8
Tyler Stroud <tyler@tylerstroud.com>
2015-10-13 07:15:23 -0700

    Update RepositoryInterface with requirements arguments

--- src/Tystr/RestOrm/Repository/RepositoryInterface.php
+++ src/Tystr/RestOrm/Repository/RepositoryInterface.php
@@ -13,31 +13,35 @@ interface RepositoryInterface
      * @param object $object
      * @param bool   $mapResponse
      * @param array  $parameters
+     * @param array  $requirements
      *
      * @return object
      */
-    public function save($object, $mapResponse = false, array $parameters = []);
+    public function save($object, $mapResponse = false, array $parameters = [], array $requirements = []);
 
     /**
      * @param string|int $id
      * @param array      $parameters
+     * @param array      $requirements
      *
      * @return object
      */
-    public function findOneById($id, array $parameters = []);
+    public function findOneById($id, array $parameters = [], array $requirements = []);
 
     /**
      * @param array $parameters
+     * @param array $requirements
      *
      * @return array|object[]
      */
-    public function findAll(array $parameters = []);
+    public function findAll(array $parameters = [], array $requirements = []);
 
     /**
      * @param object $object
      * @param array  $parameters
+     * @param array  $requirements
      *
      * @return bool
      */
-    public function remove($object, array $parameters);
+    public function remove($object, array $parameters, array $requirements = []);
 }
